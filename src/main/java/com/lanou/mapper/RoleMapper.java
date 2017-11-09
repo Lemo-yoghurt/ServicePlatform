df78f84dbec2df9b70b44781b831e6c7cb709ccc
zyh127 <136309326@qq.com>
2017-11-09 21:54:10 +0800

    修改了前端页面的左侧菜单，增加了角色列表的显示全部和跳转到角色的添加页面，进一步加深了git提交和处理冲突的理解。

---
+++ src/main/java/com/lanou/mapper/RoleMapper.java
@@ -0,0 +1,23 @@
+package com.lanou.mapper;
+
+import com.lanou.bean.Role;
+
+import java.util.List;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKeyWithBLOBs(Role record);
+
+    int updateByPrimaryKey(Role record);
+
+    List<Role> findAllRole();
+}
---
+++ src/main/java/com/lanou/service/RoleService.java
@@ -0,0 +1,20 @@
+package com.lanou.service;
+
+import com.lanou.bean.Role;
+
+import java.util.List;
+
+/**
+ *
+ * @author dllo
+ * @date 2017/11/9
+ */
+public interface RoleService {
+
+    /**
+     * 显示所有
+     * @return List<Role>
+     */
+    List<Role> getAllRole();
+
+}
